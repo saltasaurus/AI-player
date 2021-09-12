39213fefda19725062fe419904c5ef1a6520178e
saltasaurus <rjulle3@lsu.edu>
2021-09-12 15:48:15 -0500

    Initial files

---
+++ main.py
@@ -0,0 +1,16 @@
+from src import game
+
+
+
+from argparse import ArgumentParser
+
+
+if __name__ == "__main__":
+
+    # Command Line Argument Parser
+    parser = ArgumentParser.add_argument(description="Artificial Intelligence Game")
+
+
+    game()
+
+
---
+++ src/Game.py
@@ -0,0 +1,14 @@
+
+
+
+
+
+class Game():
+
+    def __init__(self, width, height):
+        self.WIDTH = width
+        self.HEIGHT = height
+
+
+def game():
+    pass
